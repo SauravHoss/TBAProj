417b65e5ffcc75009bfb637bfaf9aff2e53a1518
Rome <Rome@Rome>
2018-01-07 23:01:08 -0500

    pls

---
+++ Roomz/src/Board.java
@@ -0,0 +1,25 @@
+public class Board 
+{
+    private Room[][] game;
+    
+	public Board(Room[][] game)
+    {
+        this.game = game;
+    }
+
+    public void printMap()
+    {
+
+        for(Room[] row : game)
+        {
+            for (Room room : row)
+            {
+                room.print();
+            }
+            System.out.println();
+        }
+    }
+    
+    
+   
+}
